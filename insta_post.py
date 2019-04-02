d0f2f68c426de47852ae243b570e57cdcc95e304
Vadim Popov <m.vadimpopov@gmail.com>
2019-04-02 13:00:57 +0300

    Create insta_post.py
    added instagram api posting module

---
+++ insta_post.py
@@ -0,0 +1,13 @@
+import glob
+import random
+import time
+from instabot import Bot
+
+def insta_post()
+    pics = glob.glob("./images/*.jpg")
+
+    bot = Bot()
+    bot.login(username=os.getenv("LOGIN"), password=os.getenv("PASSWORD"))
+    for pic in pics:
+        bot.upload_photo(pic)
+        time.sleep(random.randint(1,5))
