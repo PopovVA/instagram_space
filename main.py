70a8f52c69a3ee3510a3798859524bb68ac409ca
Vadim Popov <m.vadimpopov@gmail.com>
2019-04-03 16:22:52 +0300

    Update main.py
    fixed instagram function and import name

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import pathlib
 import fetch_hubble
 import fetch_spacex
-import insta_post
+import insta
 
 
 files_directory= "images/"
@@ -16,7 +16,7 @@ fetch_spacex.fetch_spacex_last_launch(url,files_directory)
 url = 'http://hubblesite.org/api/v3/image/{}'
 fetch_hubble.fetch_hubble_images(url,files_directory,1)
 
-insta_post.insta_post()
+insta.create_insta_post()
 
 
 
