c829e9efb5d96628a90aa4cf7c494bca1fb6d290
Vadim Popov <m.vadimpopov@gmail.com>
2019-04-03 16:28:30 +0300

    Update fetch_spacex.py
    fixed image_name_tmp to image_name_tmpl

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -8,8 +8,8 @@ def fetch_spacex_last_launch(url,path):
     count = 0
     for url_image in lstest_images:
         image = requests.get(url_image)
-        image_name_tmp = "images/spacex{}{}"
+        image_name_tmpl = "images/spacex{}{}"
         count+=1
-        image_name = image_name_tmp.format(count,'.jpg')
+        image_name = image_name_tmpl.format(count,'.jpg')
         with open(image_name, 'wb') as file:
              file.write(image.content)
