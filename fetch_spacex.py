6b2b228e1628b1fa4be1273150191785eeca64e4
Vadim Popov <m.vadimpopov@gmail.com>
2019-04-04 14:38:29 +0300

    Update fetch_spacex.py
    added enumerate function

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -5,11 +5,9 @@ def fetch_spacex_last_launch(url,path):
     response = requests.get(url).json()
     latest_images = response['links']['flickr_images']
     
-    count = 0
-    for url_image in latest_images:
+    for index,url_image in enumerate(latest_images):
         image = requests.get(url_image)
         image_name_tmpl = "images/spacex{}{}"
-        count+=1
-        image_name = image_name_tmpl.format(count,'.jpg')
+        image_name = image_name_tmpl.format(index,'.jpg')
         with open(image_name, 'wb') as file:
              file.write(image.content)
